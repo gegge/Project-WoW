4fd086f9c87905e891997b223a21e317cdae2077
Fabian <fabian@multi-emu.com>
2015-10-28 22:12:56 +0100

    Some basic stuff for consulo.

---
+++ Projects/Framework/Framework/Database/DB.cs
@@ -0,0 +1,9 @@
+// Copyright (c) Arctium Emulation.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Framework.Database
+{
+    public class DB
+    {
+    }
+}
---
+++ Projects/Servers/RemoteServer/RemoteServer/RemoteServer.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Arctium Emulation.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace RemoteServer
+{
+    public class RemoteServer
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
